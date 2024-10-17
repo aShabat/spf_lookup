ba43c842352d4f666d68271bb668a17f066e2e88
Anton Shabat <anton.shabat@gmail.com>
2024-10-17 11:53:05 +0300

    removed spf-lookup

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,25 @@
-import { getDnsRecords } from "@layered/dns-records"
+import { toASCII } from 'punycode'
 
+async function getDnsRecords(name, type) {
+	const re = await fetch(`https://dns.google/resolve?name=${toASCII(name)}&type=${type}&cd=1`)
+
+	if (!re.ok) {
+		throw new Error(`Error fetching DNS records for ${name}: ${re.status} ${re.statusText}`)
+	}
+
+	const json = await re.json()
+	const records = (json.Answer || []).map((record) => {
+		let data = record.data
+
+		return {
+			name: record.name,
+			ttl: record.TTL,
+			data,
+		}
+	})
+
+	return records
+}
 function domain_from_url(link) {
   const url = new URL(link)
   const hostname = url.hostname
